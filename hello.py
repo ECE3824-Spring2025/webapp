c880b5f70e2270ad2ed5521611a0a609ba6524b7
Yi Yang <yiyang@yis-mac.mynetworksettings.com>
2025-01-22 10:29:29 -0500

    first commit

---
+++ hello.py
@@ -0,0 +1,28 @@
+from flask import Flask, request
+
+app = Flask(__name__, static_folder='web/static', static_url_path='',)
+
+@app.route("/api/hello")
+def helloworld():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8080)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# @app.route("/api/data")
+# def getdata():
+#     return {"message":"You clicked!"}
+
+
